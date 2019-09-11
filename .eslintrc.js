6eb2c48e34076c821e09788c185a77f5a406a67a
Henrique Tavares <ihenrits@gmail.com>
2019-09-10 23:51:33 -0300

    Rotas privadas

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,9 @@ module.exports = {
     camelcase: "off",
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": [{
+      "html": "ignore",
+  }]
   },
 };
