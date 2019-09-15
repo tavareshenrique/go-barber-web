09a1c792c2855b99eb4cc219f599132c4ce88c54
Henrique Tavares <ihenrits@gmail.com>
2019-09-15 00:41:32 -0300

    Validaçoes com YUP

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -19,46 +19,53 @@ export const Content = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 30px;
-  }
 
-  input {
-    background: rgba(0, 0, 0, 0.1);
-    border: 0;
-    border-radius: 4px;
-    height: 44px;
-    padding: 0 15px;
-    color: #fff;
-    margin: 0 0 10px;
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
 
-    &::placeholder {
-      color: rgba(255, 255, 255, 0.7);
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
     }
-  }
 
-  button {
-    margin: 5px 0 0;
-    height: 44px;
-    background: #3b9eff;
-    font-weight: bold;
-    color: #fff;
-    border: 0;
-    border-radius: 4px;
-    font-size: 16px;
-    transition: background 0.2s;
-
-    &:hover {
-      background: ${darken(0.03, '#3b9eff')};
+    span {
+      color: #fb6f91;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
     }
-  }
 
-  a {
-    color: #fff;
-    margin-top: 15px;
-    font-size: 16px;
-    opacity: 0.8;
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #3b9eff;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
 
-    &:hover {
-      opacity: 1;
+      &:hover {
+        background: ${darken(0.03, '#3b9eff')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
     }
   }
 `;
