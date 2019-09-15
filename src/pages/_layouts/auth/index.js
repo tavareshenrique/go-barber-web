102c4b666dc5a8642b3f2a89fe03d319e5a1089f
Henrique Tavares <ihenrits@gmail.com>
2019-09-14 23:58:19 -0300

    Utilizando Root Import

--- src/pages/_layouts/auth/index.js
+++ src/pages/_layouts/auth/index.js
@@ -4,13 +4,9 @@ import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
 
 export default function AuthLayout({ children }) {
-  return (
-    <Wrapper>
-      {children}
-    </Wrapper>
-  );
+  return <Wrapper>{children}</Wrapper>;
 }
 
 AuthLayout.propTypes = {
   children: PropTypes.element.isRequired,
-}
+};
