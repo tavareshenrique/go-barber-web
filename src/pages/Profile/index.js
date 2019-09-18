af9be7f5f9fd57b8dc2fc9dc6723203705445c77
Henrique Tavares <ihenrits@gmail.com>
2019-09-17 23:13:16 -0300

    Pagina de Perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova Senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de Senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair</button>
+    </Container>
+  );
 }
