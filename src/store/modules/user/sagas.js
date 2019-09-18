be28be88ed8097518f277f2088759537a1e9c8e5
Henrique Tavares <ihenrits@gmail.com>
2019-09-18 00:24:42 -0300

    Foto de Perfil

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -6,15 +6,14 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
     const profile = {
       name,
       email,
+      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
-
-    console.log(profile);
 
     const response = yield call(api.put, 'users', profile);
 
